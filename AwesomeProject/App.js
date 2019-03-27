1548987f0ed746ad8e0a9dd5ef5af612be0f44d3
ekchhuon <chhuon.ek@gmail.com>
2019-03-27 13:16:48 +0700

    Add Pi Pay Screen

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,49 +1,70 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Button, TouchableOpacity} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native, Hello world!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <Image style = {styles.image} source={require('./img/pipay-logo.png')} />
+        <Text style={styles.title}>Pi Pay 2.0 is Available Now!</Text>
+        <Text style={styles.description}>The new Pi Pay app brings exciting new features and performance improvements! Upgrade now to experience the world of infinite possibilities!</Text>
+              
+      <TouchableOpacity style = {styles.button}>
+        <Text style = {styles.buttonText}>Update Now</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style = {styles.noThanksText}>No, Thanks</Text>
+      </TouchableOpacity>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  noThanksText:{
+    marginTop:15,
+    color:'#333',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FFF',
   },
-  welcome: {
+  title: {
     fontSize: 20,
+    color:'#333',
+    fontWeight:'700',
     textAlign: 'center',
-    margin: 10,
+    margin: 20,
+    marginLeft: 70,
+    marginRight: 70 
+
   },
-  instructions: {
+  description: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+    marginLeft: 65,
+    marginRight: 65,
+    lineHeight:22
   },
+  image:{
+    width: 90, 
+    height: 90
+  }, 
+  button:{
+    marginTop:40,
+    width: 270, 
+    height: 47,
+    backgroundColor: '#ED288A',
+    justifyContent:'center',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+  buttonText:{
+    fontWeight:'800',
+    color:'#fff'
+  }
 });
